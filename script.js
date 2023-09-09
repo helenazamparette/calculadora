982bf9148d619b24b63b4f1fe7049fa7bf0e15db
Helena Zamparette <127434407+helenazamparette@users.noreply.github.com>
2023-09-09 18:32:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//tipos de função
+
+//declarativas
+/*function funcao() {
+    console.log('Tudo certo jovem!'); 
+}
+
+funcao()*/
+
+//expressão de função
+//com nomeação
+
+/*var funcao = function () {
+    console.log('Sou uma mensagem de função de expressão');
+}
+
+funcao();*/
+
+//arrow function
+/*var funcao = () => {
+    console.log ('Sou uma arrow function!');
+}
+
+funcao();*/
+
+
+
